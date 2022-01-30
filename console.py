ec7259b75a387ce1a1e741a3ce16b5859d58d245
MoraaOnduso <mary.ondusoo@gmail.com>
2022-01-30 03:03:03 -0800

    Console 0.0.1

---
+++ console.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""entry point for command interpreter"""
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    """Implements the console for this project"""
+
+    prompt = "(hbnb) "
+
+    def do_quit(self, argv):
+        """exit the console on execution"""
+        return True
+
+    def do_EOF(self, argv):
+        """exits the program after receiving EOF signal."""
+        return True
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
